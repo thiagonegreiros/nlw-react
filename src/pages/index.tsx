7a8ce93874276c3514956c14eb6a98a70bbee860
Thiago Negreiros <negreiros.t@gmail.com>
2021-07-28 13:24:17 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,165 @@
+import { GetStaticProps } from "next";
+import Image from "next/image"
+import Link from "next/link"
+import Head from "next/head"
+import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR"
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+import styles from "./home.module.scss";
+import { usePlayer } from "../contexts/PlayerContext";
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+}
+
+type HomeProps = {
+  lastestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({ lastestEpisodes, allEpisodes }: HomeProps) {
+  const { playList } = usePlayer();
+
+  const episodeList = [...lastestEpisodes, ...allEpisodes];
+
+
+  return (
+    <div className={styles.homepage}>
+
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
+
+      <section className={styles.lastetEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {lastestEpisodes.map((episode, index) => {
+            return (
+              <li key={episode.id}>
+                {/* Essa é a altura e largura que eu vou carregar a imagem */}
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.epsisodeDetails}>
+                  {/* O component Link faz com que eu continue com SPA na pagina */}
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button" onClick={() => playList(episodeList, index)} >
+                  <img src="/play-green.svg" alt="Tocar Episodio" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos os episodios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((episode, index) => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover" />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button" onClick={() => playList(episodeList, index + lastestEpisodes.length)}>
+                      <img src="/play-green.svg" alt="Tocar episodio" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  )
+}
+
+//? SSG -> Static Site Generation 
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc"
+    }
+  });
+
+  const episodies = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", { locale: ptBr }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
+
+  //? Mostra da posição 0 até a dois
+  const lastestEpisodes = episodies.slice(0, 2);
+
+  //? Mostra da posição 2 até o último elemento do array
+  const allEpisodes = episodies.slice(2, episodies.length);
+
+  return {
+    props: {
+      lastestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8, //? A cada 8 horas o será feita a chamada na API
+  }
+}
